b3c30961f8ca1808a69910396b0f1643ca61cc1e
Maxime Plantady <maxime.plantady@gmail.com>
2018-05-27 06:37:10 -0400

    clean code, reduce detected object to 5 and change threshold dynamcally

--- foodwaste.py
+++ foodwaste.py
@@ -15,11 +15,6 @@ def getCategory(classId):
         return database[str(classId)]['name']
     return ''
 
-def getStorageDesc(classId):
-    if str(classId) in database:
-        return database[str(classId)]['storageDesc']
-    return ''
-
 def getImagePath(classId, index):
     if str(classId) in database:
         return database[str(classId)]['image' + str(index)]
@@ -34,8 +29,10 @@ pygame.init()
 pygame.display.set_caption("Freshest.AI")
 screen = pygame.display.set_mode([screenWidth,screenHeight])
 
-index = 0;
+indexUI = 0;
+font = cv2.FONT_HERSHEY_SIMPLEX
 
+ # Load Tensorflow model
 with tf.gfile.Open('frozen_inference_graph.pb', 'rb') as f:
     graph_def = tf.GraphDef()
     graph_def.ParseFromString(f.read())
@@ -57,40 +54,57 @@ with tf.Session() as sess:
 
             inp = cv2.resize(frame, (cols, rows))
 
-            # Run TF model
+            # Run Tensorflow model
             out = sess.run([sess.graph.get_tensor_by_name('num_detections:0'),
                             sess.graph.get_tensor_by_name('detection_scores:0'),
                             sess.graph.get_tensor_by_name('detection_boxes:0'),
                             sess.graph.get_tensor_by_name('detection_classes:0')],
                            feed_dict={'image_tensor:0': inp.reshape(1, inp.shape[0], inp.shape[1], 3)})
 
-            font = cv2.FONT_HERSHEY_SIMPLEX
-
-            # Display bounding box and UI
             num_detections = int(out[0][0])
+            filteredIndex = set()
+            classDelectedList = []
+
+            # Filter sorted detected item index by score
             for i in range(num_detections):
 
                 classId = int(out[3][0][i])
                 score = float(out[1][0][i])
+                category = getCategory(classId)
+
+                # if a classId already been detected threshold is higher
+                threshold = 0.1 + classDelectedList.count(classId) * 0.1
+
+                if score > threshold and category != '':
+                    filteredIndex.add(i)
+                    classDelectedList.append(classId)
+
+                    if len(filteredIndex) > 5:
+                        break
+            
+            # Display bounding box and UI
+            for i in filteredIndex:
+
+                classId = int(out[3][0][i])
                 bbox = [float(v) for v in out[2][0][i]]
                 category = getCategory(classId)
-                if score > 0.1 and category != '':
-                    x = bbox[1] * cols
-                    y = bbox[0] * rows
-                    right = bbox[3] * cols
-                    bottom = bbox[2] * rows
-                    cv2.rectangle(inp, (int(x), int(y)), (int(right), int(bottom)), (125, 255, 51), thickness=1)
 
-                    if (len(group) == 0):
-                        image = pygame.image.load(getImagePath(classId, index+1))
-                        sprite = pygame.sprite.Sprite()
-                        sprite.image = image
-                        sprite.rect = image.get_rect()
-                        sprite.rect.x = max(0, min(x, screenWidth - sprite.rect.width))
-                        sprite.rect.y = max(0, min(y, screenHeight - sprite.rect.height))
-                        group.add(sprite)
-                    else:
-                        cv2.putText(inp, category, (int(x), int(y)), font, 0.5, (255,255,255), 2, cv2.LINE_AA)
+                x = bbox[1] * cols
+                y = bbox[0] * rows
+                right = bbox[3] * cols
+                bottom = bbox[2] * rows
+                cv2.rectangle(inp, (int(x), int(y)), (int(right), int(bottom)), (125, 255, 51), thickness=1)
+
+                if (len(group) == 0):
+                    image = pygame.image.load(getImagePath(classId, indexUI+1))
+                    sprite = pygame.sprite.Sprite()
+                    sprite.image = image
+                    sprite.rect = image.get_rect()
+                    sprite.rect.x = max(0, min(x, screenWidth - sprite.rect.width))
+                    sprite.rect.y = max(0, min(y, screenHeight - sprite.rect.height))
+                    group.add(sprite)
+                else:
+                    cv2.putText(inp, category, (int(x), int(y)), font, 0.5, (255,255,255), 2, cv2.LINE_AA)
                   
 
             screen.fill([0,0,0])
@@ -108,7 +122,7 @@ with tf.Session() as sess:
                 if event.type == KEYDOWN:
                     sys.exit(0)
                 if event.type == pygame.MOUSEBUTTONUP:
-                    index = (index + 1) % 4
+                    indexUI = (indexUI + 1) % 4
 
     except KeyboardInterrupt:
         pygame.quit()
